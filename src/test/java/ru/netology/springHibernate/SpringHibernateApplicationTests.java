b03b79f8b54482119c184ac840f8f84793bc7f4b
Vadim Persidskii <vadim@example.com>
2025-03-30 17:02:13 +0300

    Initial commit

---
+++ src/test/java/ru/netology/springHibernate/SpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
